c6fd22f1fb03260fc9782213e98ceb6bd949e350
Martin Herting <herting@uscreen.de>
2022-02-02 11:45:11 +0100

    Move module code to esm.

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
-const path = require('path')
-const cli = require('commander')
-const readPkgUp = require('read-pkg-up')
-const writePackage = require('write-pkg')
-const fs = require('fs-extra')
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { program } from 'commander'
+import { readPackageUpSync } from 'read-pkg-up'
+import { writePackage } from 'write-pkg'
+import fs from 'fs-extra'
 
-const { spawn } = require('child_process')
+import { spawn } from 'child_process'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 let root
 let skeleton
@@ -44,7 +48,7 @@ const initializeYarn = (path) =>
  * install extra dev packages from skeleleton
  */
 const installDevPackages = (appPath, skelPath) => {
-  const skelPack = readPkgUp.sync({ cwd: skelPath })
+  const skelPack = readPackageUpSync({ cwd: skelPath })
   const devDependencies = Object.keys(skelPack.packageJson.devDependencies)
   return new Promise((resolve, reject) => {
     const yarn = spawn('yarn', ['add', ...devDependencies, '-D'], {
@@ -63,7 +67,7 @@ const installDevPackages = (appPath, skelPath) => {
  * install extra prod packages from skeleleton
  */
 const installPackages = (appPath, skelPath) => {
-  const skelPack = readPkgUp.sync({ cwd: skelPath })
+  const skelPack = readPackageUpSync({ cwd: skelPath })
   const dependencies = Object.keys(skelPack.packageJson.dependencies)
   return new Promise((resolve, reject) => {
     const yarn = spawn('yarn', ['add', ...dependencies], {
@@ -82,8 +86,8 @@ const installPackages = (appPath, skelPath) => {
  * configure package.json to use linting, testing, stuff
  */
 const addPackageConfig = (path, skelPath) => {
-  const skelPack = readPkgUp.sync({ cwd: skelPath })
-  const pack = readPkgUp.sync({ cwd: path })
+  const skelPack = readPackageUpSync({ cwd: skelPath })
+  const pack = readPackageUpSync({ cwd: path })
   delete pack.packageJson._id
   delete pack.packageJson.readme
 
@@ -132,7 +136,7 @@ const copyEnv = (appPath, skelPath) => {
 /**
  * define the command
  */
-cli
+program
   .version('0.1.0')
   .arguments('<name> [opt]')
   .action(async (name, opt) => {
@@ -166,11 +170,11 @@ cli
 /**
  * read args
  */
-cli.parse(process.argv)
+program.parse(process.argv)
 
 /**
  * output help as default
  */
 if (!process.argv.slice(2).length) {
-  cli.help()
+  program.help()
 }
