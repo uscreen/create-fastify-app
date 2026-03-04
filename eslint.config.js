9a67a5e475d18cec683c5e39219cd6535e24824a
Martin Herting <herting@uscreen.de>
2026-03-04 17:29:19 +0100

    Use @antfu/eslint-config.

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  formatters: true,
+  ignores: [
+    'skeleton/',
+    'coverage/**',
+    '*.d.ts',
+    'pnpm-workspace.yaml',
+    '**/*.md/*.js',
+    'AGENTS.md'
+  ]
+}, {
+  rules: {
+    'style/comma-dangle': ['error', 'never'],
+    'curly': ['error', 'multi-line', 'consistent'],
+    'antfu/top-level-function': 'off',
+    'no-console': 'off',
+    'test/no-import-node-test': 'off'
+  }
+})
